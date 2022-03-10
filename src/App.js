254ad3ad0cb9099a910dfdc914c3db097d6d8a85
SUNIL <86763715+ZEROSOFTWAREDEVELOPER@users.noreply.github.com>
2022-03-11 03:29:01 +0530

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the capital of India?',
+			answerOptions: [
+				{ answerText: 'lucknow', isCorrect: false },
+				{ answerText: 'Jaipur', isCorrect: false },
+				{ answerText: 'Delhi', isCorrect: true },
+				{ answerText: 'pune', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many States Are Present In India?',
+			answerOptions: [
+				{ answerText: '21', isCorrect: false },
+				{ answerText: '24', isCorrect: false },
+				{ answerText: '26', isCorrect: false },
+				{ answerText: '29', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+
+			setScore(score + 1);
+
+		}
+
+		const nextQuestion = currentQuestion + 1;
+
+		if (nextQuestion < questions.length) {
+
+			setCurrentQuestion(nextQuestion);
+
+		} else {
+
+			setShowScore(true);
+
+		}
+	};
+	return (
+
+		<div>
+			{showScore ? (
+
+
+
+				<div >
+		 scored {score} out of {questions.length}
+
+
+				</div>
+	) : (    
+				<>
+<div>
+		<div>
+			<span>Question
+
+		{currentQuestion + 1}</span>/
+								 
+	{questions.length}
+		</div>
+			
+		<div>
+
+		{questions[currentQuestion].questionText}</div>
+
+		</div>
+
+		<div >
+
+
+
+		{questions[currentQuestion].answerOptions.map((answerOption) => 
+
+						(
+
+		<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+
+								
+	{answerOption.answerText}
+		</button>
+	))}
+	</div>
+</>
+)}
+</div>
+	);
 }
-
-export default App;
